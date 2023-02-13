585f586761d16ac2988f1e080988e03663ea4e48
Darafobia <120211900+Darafobia@users.noreply.github.com>
2023-02-13 13:41:01 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var player, playerAnimation;
+
+
+function preload(){
+  playerAnimation=loadAnimation("running.png","running2");
+}
+
+function setup() {
+  createCanvas(1000,1000);
+    player=createSprite(500,500,10,10);
+}
+
+function draw() {
+
+}
+
